7d90d4c30908d00f9b09b78902b8aa7464c7e6c3
Borlon <imagirlfukk@gmail.com>
2024-06-03 11:00:27 +0800

    Amen

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # command first input
+    print( "os paging replacement" )
+    print(" input command: \t")
+    command = int(input())
+    while command != 0:
+        if command == 1:
+
+
+
+
